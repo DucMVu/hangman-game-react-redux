c63354a12b3a95a439098fac7d5b4eba3215ce60
DucMVu <vmd0210@gmail.com>
2019-07-05 15:08:52 +0200

    update redux devtools

--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__()
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
 
 const enhancer = compose(applyMiddleware(ReduxThunk),devTools)
 
